a163f366b8c75db12b776592a9401be74aab5ef6
Shadrack <128532446+shackro@users.noreply.github.com>
2026-03-19 16:47:49 -0400

    Update CORS allowed origins for Afroconnect

--- backend/settings.py
+++ backend/settings.py
@@ -142,8 +142,8 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 CORS_ALLOWED_ORIGINS = [
     'https://cryonexes.com',
     'https://www.cryonexes.com',
-    'afroconnect.vercel.app',
-    'afroconnect-drab.vercel.app',
+    'https://afroconnect.vercel.app',
+    'https://afroconnect-drab.vercel.app',
     'https://cryonexes-frontend.onrender.com',  # Your frontend URL
     'http://localhost:5173',
     'http://localhost:3000',
